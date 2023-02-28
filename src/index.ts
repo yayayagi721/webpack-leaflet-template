209816d9d03ce103a192727f5f20fbc5d1800383
yayayagi721 <snoopy1995.1.25@gmail.com>
2023-02-28 22:54:18 +0900

    add leaflet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import L from "leaflet";
+import "leaflet-css";
 import "./style.scss";
 
-console.log(window);
+var map = L.map("map-container");
+map.setView([35.4, 136], 5);
+L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
+  attribution:
+    "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
+}).addTo(map);
